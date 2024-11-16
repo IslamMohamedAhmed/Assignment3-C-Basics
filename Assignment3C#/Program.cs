7ab1e4b5c6c68466bbf1d80488e678391d725d00
IslamMohamedAhmed <islammohamedahmed49@gmail.com>
2024-11-16 14:17:02 +0200

    c# basics

---
+++ Assignment3C#/Program.cs
@@ -0,0 +1,302 @@
+﻿using System.Reflection.Metadata;
+
+namespace Assignment3C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Assignment3.1
+            //Console.WriteLine("Enter a Number!");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //if(num % 3 == 0 && num % 4 == 0)
+            //{
+            //    Console.WriteLine("Yes");
+            //}
+            //else
+            //{
+            //  Console.WriteLine("No");
+            //}
+            #endregion
+
+            #region Assignment3.2
+            //Console.WriteLine("Enter a Number!");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //if(num > 0)
+            //{
+            //    Console.WriteLine("Positive");
+            //}
+            //else if(num < 0){
+            //    Console.WriteLine("Negative");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Zero");
+            //}
+            #endregion
+
+            #region Assignment3.3
+            //Example 1
+            //Console.WriteLine("Enter three integers, separate them by comma!");
+            //string str1 = Console.ReadLine();
+            //var arr1 = str1.Split(',');
+            //int x = int.Parse(arr1[0]);
+            //int y = int.Parse(arr1[1]);
+            //int z = int.Parse(arr1[2]);
+            //Console.WriteLine($"max element = {Math.Max(x, Math.Max(y, z))}");
+            //Console.WriteLine($"min element = {Math.Min(x, Math.Min(y, z))}");
+
+
+            //Example 2
+            //Console.WriteLine("Enter three integers, separate them by space!");
+            //string str1 = Console.ReadLine();
+            //var arr1 = str1.Split(' ');
+            //int x = int.Parse(arr1[0]);
+            //int y = int.Parse(arr1[1]);
+            //int z = int.Parse(arr1[2]);
+            //Console.WriteLine($"max element = {Math.Max(x, Math.Max(y, z))}");
+            //Console.WriteLine($"min element = {Math.Min(x, Math.Min(y, z))}");
+            #endregion
+
+            #region Assignment3.4
+            //Console.WriteLine("Enter a Number!");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine((num % 2 == 0) ? "even" : "odd");
+            #endregion
+
+            #region Assignment3.5
+            //Console.WriteLine("Enter a Character!");
+            //char ch = Console.ReadLine()[0];
+            //switch (char.ToLower(ch))
+            //{
+            //    case 'a':
+            //    case 'e':
+            //    case 'i':
+            //    case 'o':
+            //    case 'u':
+            //        Console.WriteLine("Vowel");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Consonant");
+            //        break;
+            //}
+            #endregion
+
+            #region Assignment3.6
+            //Console.WriteLine("Enter a Number!");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //for(int i = 1; i <= num; i++)
+            //{
+            //    if(i == num)
+            //    {
+            //        Console.Write($"{i}\n");
+            //    }
+            //    else
+            //    {
+            //        Console.Write($"{i}, ");
+            //    }
+            //}
+            #endregion
+
+            #region Assignment3.7
+            //Console.WriteLine("Enter a Number!");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 1; i <= 12; i++)
+            //{
+            //    Console.Write($"{i * num} ");
+            //}
+            //Console.WriteLine();
+            #endregion
+
+            #region Assignment3.8
+            //Console.WriteLine("Enter a Number!");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 1; i <= num; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.Write($"{i} ");
+            //    }
+
+            //}
+            //Console.WriteLine();
+            #endregion
+
+            #region Assignment3.9
+            //Console.WriteLine("Enter a Number then its power!");
+            //string str = Console.ReadLine();
+            //var arr = str.Split(" ");
+            //int num = int.Parse(arr[0]);
+            //int power = int.Parse(arr[1]);
+            //Console.WriteLine(Math.Pow(num, power));
+            #endregion
+
+            #region Assignment3.10
+            //Console.WriteLine("Enter marks of 5 subjects!");
+            //string str = Console.ReadLine();
+            //var arr = str.Split(" ");
+            //int[] arr2 = new int[5];
+            //for(int i = 0; i < 5; i++)
+            //{
+            //    arr2[i] = Convert.ToInt32(arr[i]);
+            //}
+            //Console.WriteLine($"total marks = {arr2.Sum()}");
+            //Console.WriteLine($"average mark = {arr2.Average()}");
+            //// by calculation we find out that average is the same as percentage so,
+            //Console.WriteLine($"percentage = {arr2.Average()} %");
+
+            #endregion
+
+            #region Assignment3.11
+            //Console.WriteLine("Enter the number of your month!");
+            //int num = int.Parse(Console.ReadLine());
+            //switch (num)
+            //{
+            //    case 1:
+            //    case 3:
+            //    case 5:
+            //    case 8:
+            //    case 10:
+            //    case 12:
+            //        Console.WriteLine("31");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("28 or 29");
+            //        break;
+            //    case 4:
+            //    case 6:
+            //    case 7:
+            //    case 9:
+            //    case 11:
+            //        Console.WriteLine("30");
+            //        break;
+            //    default:
+            //        Console.WriteLine("This month is unknown or doesn't exist!");
+            //        break;
+
+            //}
+            #endregion
+
+            #region Assignment3.12
+            //Console.WriteLine("inserted your equation separated by spaces!");
+            //string str = Console.ReadLine();
+            //var arr = str.Split(" ");
+            //double fNum = Convert.ToDouble(arr[0]);
+            //char sign = arr[1][0];
+            //double sNum = Convert.ToDouble(arr[2]);
+            //switch (sign)
+            //{
+            //    case '+':
+            //        Console.WriteLine(fNum+sNum);
+            //        break;
+            //    case '-':
+            //        Console.WriteLine(fNum - sNum);
+            //        break;
+            //    case '*':
+            //        Console.WriteLine(fNum * sNum);
+            //        break;
+            //    case '/':
+            //        Console.WriteLine(fNum / sNum);
+            //        break;
+            //    case '%':
+            //        Console.WriteLine(fNum % sNum);
+            //        break;
+            //    default:
+            //        Console.WriteLine("wrong equation !");
+            //        break;
+            //}    
+
+            #endregion
+
+            #region Assignment3.13
+            //string originalString = "Hello World!";
+            //Console.WriteLine("Enter the string you want to reverse!");
+            //string str = Console.ReadLine();
+            //char[] charArray = str.ToCharArray();
+            //Array.Reverse(charArray);
+            //string reversedString = new string(charArray);
+            //Console.WriteLine("Original string: " + str);
+            //Console.WriteLine("Reversed string: " + reversedString);
+            #endregion
+
+            #region Assignment3.14
+            //Console.WriteLine("Enter the Number you want to reverse!");
+            //string str = Console.ReadLine();
+            //char[] charArray = str.ToCharArray();
+            //Array.Reverse(charArray);
+            //string reversedString = new string(charArray);
+            //Console.WriteLine("Original number: " + str);
+            //Console.WriteLine("Reversed number: " + reversedString);
+            #endregion
+
+            #region Assignment3.15
+            //      // Get start and end numbers from the user
+            //      int startNum, endNum;
+
+            //      Console.WriteLine("Enter the starting number: ");
+            //      startNum = Convert.ToInt32(Console.ReadLine());
+
+            //      Console.WriteLine("Enter the ending number: ");
+            //      endNum = Convert.ToInt32(Console.ReadLine());
+
+            //      // Ensure starting number is at least 2
+            //      startNum = Math.Max(startNum, 2);
+
+            //      Console.WriteLine("Prime numbers between {0} and {1} are:", startNum, endNum);
+
+            //      // Find and display prime numbers
+            //      for (int num = startNum; num <= endNum; num++)
+            //      {
+            //          if (IsPrime(num))
+            //          {
+            //              Console.Write("{0} ", num);
+            //          }
+            //      }
+
+            //      Console.WriteLine(); // Add a newline character after output
+            //  }
+            //static bool IsPrime(int number)
+            //      {
+            //      bool test = false;
+            //          if (number <= 1) return false;
+            //          if (number <= 3) return true;
+
+            //          // Only check divisibility by odd numbers from 3 to square root of number
+            //          for (int i = 2; i <= number/2; i ++)
+            //          {
+            //          if (number % i == 0) {
+            //              test = false;
+            //              break;
+            //          }
+            //          else
+            //          {
+            //              test = true;
+            //          }
+            //          }
+
+            //      return test;
+            //      }
+            #endregion
+
+            #region Assignment3.16
+            //Console.WriteLine("Enter your Number!");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //string binaryString = "";
+
+            //if (num == 0)
+            //{
+            //    binaryString = "0";                
+            //}
+
+            //while (num > 0)
+            //{
+            //    binaryString = (num % 2) + binaryString; // Prepend remainder (0 or 1)
+            //    num /= 2; // Divide by 2 for next iteration
+            //}
+
+            //Console.WriteLine(binaryString);
+            #endregion
+        }
+    }
+}
+
---
+++ demo_session3_c#/Program.cs
@@ -0,0 +1,229 @@
+﻿using System;
+
+namespace demo_session3_c_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region FractionsAndDiscard
+            // by default fractions are declared as double
+            //double x = 2.3;
+            //float y = 7.8f;
+            //decimal z = 345.678m;
+            //long a = 100_000_000_000; // discard
+            //Console.WriteLine($"{x} {y} {z} {a}");
+            #endregion
+
+            #region ImplicitAndExplicitCastingForValueType
+            ////int x = 10;
+            ////long y = x; //Implicit Casting [Safe Casting]
+            ////Console.WriteLine($"{x} {y}");
+            //long x = 15;
+            //int y = (int)x; //Explicit Casting
+            //Console.WriteLine($"{x} {y}");
+
+            //long x = 1000_000_000_0000;
+            //int y = (int)x;
+            //Console.WriteLine(y); // garbage value
+            //checked
+            //{
+            //    long x = 1000_000_000_0000;
+            //    int y = (int)x;
+
+            //    unchecked
+            //    {
+            //        Console.WriteLine(y);
+            //    }
+            //}
+
+
+
+            #endregion
+
+            #region UnaryOperators
+            //int x = 10;
+            //// Prefix => [Increment or Decrement then Print]
+            //Console.WriteLine(++x); // 11
+            //Console.WriteLine(--x); // 10
+            //// Postfix => [Print then Increment or Decrement]
+            //Console.WriteLine(x++); //10
+            ////then it wil add 1 to x
+            //Console.WriteLine(x--); // 11
+            #endregion
+
+            #region BinaryOrArithmeticOperators
+            //// +,-,*,/,%
+            //int x = 49;
+            //int y = 7;
+            //Console.WriteLine(x + y); // 56
+            //Console.WriteLine(x - y); // 42
+            //Console.WriteLine(x * y); // 343
+            //Console.WriteLine(x / y); // 7
+            //Console.WriteLine(x % y); // 0
+
+            #endregion
+
+            #region AssignmentOperators
+            //int x = 2;
+            //x += 2; // 4
+            //x -= 2; // 2
+            //x *= 2; // 4
+            //x /= 2; // 2
+            //x %= 2; // 0
+            #endregion
+
+            #region RelationalOperators
+            //int x = 6, y = 7;
+            //Console.WriteLine(x == y); // false
+            //Console.WriteLine(y != x); // true
+            //Console.WriteLine(x <= y); // true
+            //Console.WriteLine(y < x); // false
+            //Console.WriteLine(x >= y); // false
+            //Console.WriteLine(y > x); // true
+            #endregion
+
+            #region LogicalOperators
+            //Console.WriteLine(!false); // true
+            //Console.WriteLine(!true); // false
+            //Console.WriteLine(false && true); // false
+            //Console.WriteLine(false || true); // true
+            //Console.WriteLine(49>7 && 16>4); // true
+
+            #endregion
+
+            #region BitwiseOperators
+            //AND
+            //int num1 = 10; //(binary: 1010);
+            //int num2 = 6;  //(binary: 0110);
+            //int result = num1 & num2; // result will be 2 (binary: 0010)
+            //Console.WriteLine(result);
+
+            //OR
+            //int num1 = 10; // (binary: 1010);
+            //int num2 = 6; // (binary: 0110);
+            //int result = num1 | num2; // result will be 14 (binary: 1110)
+            //Console.WriteLine(result);
+
+            //XOR
+            //int num1 = 10; // (binary: 1010);
+            //int num2 = 6; // (binary: 0110);
+            //int result = num1 ^ num2; // result will be 12 (binary: 1100)
+            //Console.WriteLine(result);
+
+
+
+
+
+
+            #endregion
+
+            #region TernaryOperator
+
+            //int number = 10;
+            //bool isEven = (number % 2 == 0) ? true : false;
+            //Console.WriteLine(isEven); // Output: true
+
+            //int age = 20;
+            //string message = (age >= 18) ? "You are an adult." : "You are underage.";
+            //Console.WriteLine(message); // Output: You are an adult.
+
+            //Nested TRernary Operator
+
+            //int grade = 85;
+            //string letterGrade = (grade >= 90) ? "A" : (grade >= 80) ? "B" : "C";
+            //Console.WriteLine(letterGrade); // Output: B
+
+
+
+
+
+            #endregion
+
+            #region OperatorsPriority
+            // Unary Prefix Operator
+            // Round Praces ()
+            // */%
+            // +-
+            //int equation = 4 * 3 + 6; // 18
+            //Console.WriteLine(equation); // 18
+            #endregion
+
+            #region StringFormatting
+            int x = 7, y = 7;
+            int prod = x * y;
+            string m1 = "Operation is : " + x + " * " + y + " = " + prod;
+            string m2 = string.Format("Operation is : {0} * {1} = {2}", x, y, prod);
+            string m3 = $"Operation is : {x} * {y} = {x * y}";
+            Console.WriteLine(m1);
+            Console.WriteLine(m2);
+            Console.WriteLine(m3);
+
+
+            #endregion
+
+            #region ControlStatementsIfAndSwitch
+            //int number = 15;
+
+            //if (number > 10)
+            //{
+            //    Console.WriteLine("The number is greater than 10.");
+            //}
+
+
+
+            //int grade = 88;
+            //if (grade >= 90)
+            //{
+            //    Console.WriteLine("Excellent! You got an A.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Good job. You got a B.");
+            //}
+
+
+            //string letter = "C";
+
+            //if (letter == "A")
+            //{
+            //    Console.WriteLine("The letter is A.");
+            //}
+            //else if (letter == "B")
+            //{
+            //    Console.WriteLine("The letter is B.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The letter is neither A nor B.");
+            //}
+
+
+            //char initial = 'D';
+
+            //switch (initial)
+            //{
+            //    case 'A':
+            //        Console.WriteLine("You chose A.");
+            //        break;
+            //    case 'B':
+            //        Console.WriteLine("You chose B.");
+            //        break;
+            //    default:
+            //        Console.WriteLine("You chose something else.");
+            //        break;
+            //}
+
+
+
+
+
+
+            #endregion
+
+
+
+
+        }
+    }
+}
